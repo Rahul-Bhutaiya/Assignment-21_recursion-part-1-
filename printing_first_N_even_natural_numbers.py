688d76074d021c36e8d7e684012cbcce21f2a8b5
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:09:42 +0530

    Update printing_first_N_even_natural_numbers.py

--- printing_first_N_even_natural_numbers.py
+++ printing_first_N_even_natural_numbers.py
@@ -1,9 +1,16 @@
 #Write a recursive python function to print first N even natural numbers.
 
+#METHOD : 1 [USING RECURSION ONLY]
 def even(n):
     if n>0:
         even(n-1)
         print(n*2,end=' ')
 
+#METHOD : 2 [USING RECURSION + LAMBDA]
+even2=lambda n:print(2) if n==1 else print(n*2,even2(n-1))
+
+#METHOD : 3 [USING ONLY LAMBDA]
+even3=lambda n:[print(x*2,end=' ')for x in range(1,n+1)]
+
 n_val=int(input('Enter N Value : '))
-even(n_val)
+even3(n_val)
