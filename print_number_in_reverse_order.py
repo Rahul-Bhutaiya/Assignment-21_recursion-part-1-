531d5354166e9caca43afe309b106b0fbf5895fb
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:07:49 +0530

    Update print_number_in_reverse_order.py

--- print_number_in_reverse_order.py
+++ print_number_in_reverse_order.py
@@ -1,5 +1,6 @@
 #Write a recursive python function to print a number in reverse order.
 
+#METHOD : 1 [USING RECURSION ONLY]
 def reverse(number,rev=0):
     if number>0:
         last=number%10
@@ -9,5 +10,11 @@ def reverse(number,rev=0):
     else:
         print('Reverse :',rev)
 
+#METHOD : 2 [USING RECURSION + LAMBDA]
+reverse3=lambda n:'' if n==0 else str(n%10)+reverse3(n//10)
+
+#METHOD : 3 [USING ONLY LAMBDA]
+reverse2=lambda n:str(n)[::-1]
+
 number=int(input('Enter a Number : '))
-reverse(number)
+print(reverse3(number))
