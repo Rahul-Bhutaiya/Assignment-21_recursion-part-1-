68c2b571b7cb9e16be57d17d2862d05c01919d8a
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:11:09 +0530

    Update printing_first_N_natural_numbers.py

--- printing_first_N_natural_numbers.py
+++ printing_first_N_natural_numbers.py
@@ -1,9 +1,16 @@
 #Write a recursive python function to print first N natural numbers.
 
+#METHOD : 1 [USING RECURSION ONLY]
 def fnatural(n):
     if n>0:
         fnatural(n-1)
         print(n)
 
+#METHOD : 2 [USING RECURSION + LAMBDA]        
+fnatural2=lambda n:print(1) if n==1 else print(n,fnatural2(n-1))
+
+#METHOD : 3 [USING ONLY LAMBDA]
+fnatural3=lambda n:[print(x+1,end=' ')for x in range(n)]
+
 n_value=int(input('Enter N Value : '))
-fnatural(n_value)
+fnatural3(n_value)
