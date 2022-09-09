5eaf2c30201eb46e1a25e47687b08cf38acbbe99
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:11:48 +0530

    Update printing_first_N_natural_numbers_rev.py

--- printing_first_N_natural_numbers_rev.py
+++ printing_first_N_natural_numbers_rev.py
@@ -1,9 +1,16 @@
 #Write a recursive python function to print first N natural numbers in reverse order
 
+#METHOD : 1 [USING RECURSION ONLY]
 def fnat_rev(n):
     if n>0:
         print(n,end='  ')
         fnat_rev(n-1)
 
+#METHOD : 2 [USING RECURSION + LAMBDA]        
+fnat_rev2=lambda n,first=1:print(first,fnat_rev2(n,first+1)) if first<n else print(n)
+
+#METHOD : 3 [USING ONLY LAMBDA]
+fnat_rev3=lambda n:[print(x,end=' ')for x in range(n,0,-1)]
+
 n_val=int(input('Enter N Value : '))
-fnat_rev(n_val)
+fnat_rev3(n_val)
