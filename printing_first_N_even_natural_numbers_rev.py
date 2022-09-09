2274169e017f0c11d2b7300559358d1dd6faa2c0
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:10:26 +0530

    Update printing_first_N_even_natural_numbers_rev.py

--- printing_first_N_even_natural_numbers_rev.py
+++ printing_first_N_even_natural_numbers_rev.py
@@ -1,9 +1,13 @@
 #Write a recursive python function to print first N even natural numbers in reverse order.
 
+#METHOD : 1 [USING RECURSION ONLY]
 def even_rev(n):
     if n>0:
         print(n*2,end=' ')
         even_rev(n-1)
 
+#METHOD : 2 [USING ONLY LAMBDA]
+even_rev2=lambda n:[print(x*2,end=' ') for x in range(n,0,-1)]
+
 n_val=int(input('Enter N Value : '))
-even_rev(n_val)
+even_rev2(n_val)
