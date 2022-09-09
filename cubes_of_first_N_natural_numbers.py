a84907d13b0c72bf640eb42ce595e8940b80f06a
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:06:20 +0530

    Update cubes_of_first_N_natural_numbers.py

--- cubes_of_first_N_natural_numbers.py
+++ cubes_of_first_N_natural_numbers.py
@@ -1,9 +1,16 @@
 #Write a recursive python function to print cubes of first N natural numbers
 
+#METHOD : 1 [USING RECURSION ONLY]
 def cubes(n):
     if n>0:
         cubes(n-1)
         print(n**3)
 
+#METHOD : 2 [USING RECURSION + LAMBDA]
+cubes2=lambda n:print(1) if n==1 else print(n**3,cubes2(n-1))
+
+#METHOD : 3 [USING ONLY LAMBDA]
+cubes3=lambda n:[print(x**3,end=' ') for x in range(1,n+1)]
+
 n_val=int(input('Enter N Value : '))
-cubes(n_val)
+cubes3(n_val)
