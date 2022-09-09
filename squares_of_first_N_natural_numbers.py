db9eef00c19f2fa12a1635deb567ca77491e6e17
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:13:57 +0530

    Update squares_of_first_N_natural_numbers.py

--- squares_of_first_N_natural_numbers.py
+++ squares_of_first_N_natural_numbers.py
@@ -1,9 +1,16 @@
 #Write a recursive python function to print squares of first N natural numbers
 
+#METHOD : 1 [USING RECURSION ONLY]
 def squares(n):
     if n>0:
         squares(n-1)
         print(n**2)
 
+#METHOD : 2 [USING RECURSION + LAMBDA]
+squares2=lambda n:print(1)if n==1 else print(n**2,squares2(n-1))
+
+#METHOD : 3 [USING ONLY LAMBDA]
+squares3=lambda n:[print((x+1)**2)for x in range(n)]
+
 n_val=int(input('Enter N Value : '))
-squares(n_val)
+squares3(n_val)
