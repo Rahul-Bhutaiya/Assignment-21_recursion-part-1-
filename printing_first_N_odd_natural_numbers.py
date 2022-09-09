e23e77a3b2c229c46347d6742545a0671700e9e2
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-09-09 20:12:37 +0530

    Update printing_first_N_odd_natural_numbers.py

--- printing_first_N_odd_natural_numbers.py
+++ printing_first_N_odd_natural_numbers.py
@@ -1,9 +1,16 @@
 #Write a recursive python function to print first N odd natural numbers
 
+#METHOD : 1 [USING RECURSION ONLY]
 def odd(n):
     if n>0:
         odd(n-1)
         print(n*2-1,end=' ')
 
+#METHOD : 2 [USING RECURSION + LAMBDA]
+odd2=lambda n:print(1)if n==1 else print(n*2-1,odd2(n-1))
+
+#METHOD : 3 [USING ONLY LAMBDA]
+odd3=lambda n:[print((x+1)*2-1)for x in range(n)]
+
 n_value=int(input('Enter N Value : '))
-odd(n_value)
+odd2(n_value)
